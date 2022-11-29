c806d5e8b51977583ebb309deb4f1e2770724fcf
JCLin0609 <as11675847@gmail.com>
2022-11-29 12:37:45 +0800

    hw1

--- utSort.h
+++ utSort.h
@@ -13,19 +13,15 @@ TEST(Sort, sortByIncreasingPerimeter)
     Triangle tri({3, 0}, {0, 4}, {0, 0}); // Perimeter = 12
 
     list<Shape *> shape_list, ans;
-    shape_list.push_back(&cir);
-    shape_list.push_back(&rect);
-    shape_list.push_back(&tri);
-    ans.push_back(&cir);
-    ans.push_back(&rect);
-    ans.push_back(&tri);
+    shape_list.push_back(&cir); shape_list.push_back(&rect); shape_list.push_back(&tri);
+    ans.push_back(&cir);        ans.push_back(&rect);        ans.push_back(&tri);
 
     // shape_list 丟入函式排序
     Sort::sortByIncreasingPerimeter(&shape_list);
 
     // ans 在本地排序
     ans.sort([](const Shape *f, const Shape *s)
-             { return f->perimeter() < s->perimeter(); });
+             { return f->perimeter() > s->perimeter(); });
 
     // 3個shapes在兩個list中的排序結果必須相同
     for (int i = 0; i < 3; i++)
@@ -33,8 +29,8 @@ TEST(Sort, sortByIncreasingPerimeter)
         //各別從list取出一個並比較
         if (ans.back()->perimeter() != shape_list.back()->perimeter())
             FAIL() << "Sorting is error.";
-        // else
-        //     cout << ans.back()->perimeter() << " ";
+        else
+            cout << ans.back()->perimeter() << " ";
         ans.pop_back();
         shape_list.pop_back();
     }
@@ -61,7 +57,7 @@ TEST(Sort, sortByDecreasingPerimeter)
     Sort::sortByDecreasingPerimeter(&shape_list);
     //在本地排序
     ans.sort([](const Shape *f, const Shape *s)
-             { return f->perimeter() > s->perimeter(); });
+             { return f->perimeter() < s->perimeter(); });
 
     //比較函式排序和本地排序及果是否相同
     for (int i = 0; i < 3; i++)
@@ -93,7 +89,7 @@ TEST(Sort, sortByIncreasingArea)
 
     Sort::sortByIncreasingArea(&shape_list);
     ans.sort([](const Shape *f, const Shape *s)
-             { return f->area() < s->area(); });
+             { return f->area() > s->area(); });
 
     for (int i = 0; i < 3; i++)
     {
@@ -124,7 +120,7 @@ TEST(Sort, sortByDecreasingArea)
 
     Sort::sortByDecreasingArea(&shape_list);
     ans.sort([](const Shape *f, const Shape *s)
-             { return f->area() > s->area(); });
+             { return f->area() < s->area(); });
 
     for (int i = 0; i < 3; i++)
     {
@@ -155,7 +151,7 @@ TEST(Sort, sortByIncreasingCompactness) // Compactness = area/perimeter
 
     Sort::sortByIncreasingCompactness(&shape_list);
     ans.sort([](const Shape *f, const Shape *s)
-             { return (f->area() / f->perimeter()) < (s->area() / s->perimeter()); });
+             { return (f->area() / f->perimeter()) > (s->area() / s->perimeter()); });
 
     for (int i = 0; i < 3; i++)
     {
